e7550126a4f47b055dda5b51d56dfb26b5c97eb6
John Ngugi <66136743+john-ngugi@users.noreply.github.com>
2023-08-21 19:05:49 +0300

    Update settings.py

--- smart_park/settings.py
+++ smart_park/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2mj1m!63v)6kocigdxfb#9)5h2lnwg7=3o-@s$j4py&8o6tjac
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.35.141']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
